c36af5f155777a969f990c9bc7bac78ed0f6e849
Asa Rudick <Shabonkerz@gmail.com>
2014-01-26 09:55:06 -0600

    Switching to laptop.
    Still writing it.

--- mybot.js
+++ mybot.js
@@ -1,22 +1,24 @@
-function new_game() {
-}
+function new_game()
+{}
 
-function make_move() {
-   var board = get_board();
+function make_move()
+{
+	var board = get_board();
 
-   // we found an item! take it!
-   if (board[get_my_x()][get_my_y()] > 0) {
-       return TAKE;
-   }
+	// we found an item! take it!
+	if ( board[ get_my_x() ][ get_my_y() ] > 0 )
+	{
+		return TAKE;
+	}
 
-   var rand = Math.random() * 4;
+	var rand = Math.random() * 4;
 
-   if (rand < 1) return NORTH;
-   if (rand < 2) return SOUTH;
-   if (rand < 3) return EAST;
-   if (rand < 4) return WEST;
+	if ( rand < 1 ) return NORTH;
+	if ( rand < 2 ) return SOUTH;
+	if ( rand < 3 ) return EAST;
+	if ( rand < 4 ) return WEST;
 
-   return PASS;
+	return PASS;
 }
 
 // Optionally include this function if you'd like to always reset to a 
@@ -26,3 +28,68 @@ function make_move() {
 //function default_board_number() {
 //    return 123;
 //}
+var SuperBot = function () {
+
+}
+
+Superbot.prototype = {
+
+	this.strategies = {
+		OFFENSIVE: 0,
+		DEFENSIVE: 1
+	};
+
+	this.paths = [];
+
+	this.strategy = this.strategies.OFFENSIVE;
+
+
+	buildPathTree: function ()
+	{
+
+		this.fruit_locations = this.getFruits();
+
+	},
+	insertPathNode: function () {
+
+	},
+	getOptimalPath: function () {
+
+	},
+	getFruits: function ()
+	{
+		var board = get_board();
+		var fruit_locations = [];
+		for ( var x = 0; x < board.length; x++ )
+		{
+			for ( var y = 0; y < board[ x ].length; y++ )
+			{
+				if ( board[ x ][ y ] > 0 ) fruit_locations.push( new FruitLocation( x, y, board[ x ][ y ] ) );
+			};
+		};
+		return fruit_locations;
+	}
+}
+
+var FruitLocation = function ( x, y, fruit )
+{
+	this.x = x;
+	this.y = y;
+	this.fruit = fruit;
+}
+var PathNode = function ( fruitlocation, fruits )
+{
+	this.fruit_location = fruitlocation;
+	this.children = [];
+	this.insert( fruits );
+}
+PathNode.prototype = {
+	insert: function ( fruits )
+	{
+		for ( var i = 0; i < fruits.length; i++ )
+		{
+			var remaining = fruits.splice( i, 1 );
+			this.children.push( new PathNode( fruits[ i ], remaining ) );
+		};
+	}
+}
